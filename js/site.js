6f04e340ed0f8cb53cb3164e214f8ebe21c86446
SJohnson <simonbjohnson@gmail.com>
2021-04-13 23:01:50 +0100

    changing map provider

--- js/site.js
+++ js/site.js
@@ -51,9 +51,8 @@ function processingComplete(){
 function createMap(postcodes){
 	let map = L.map('map').setView([52,0], 6);
 
-	L.tileLayer('http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-	  maxZoom: 18,
-	  id: 'examples.map-i86knfo3'
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 
 	L.esri.featureLayer({
