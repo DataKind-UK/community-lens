75cd4c9752c81e1e7f6b247e9bc0e6873ff7793f
SJohnson <simonbjohnson@gmail.com>
2021-04-19 14:06:23 +0100

    Updating map for zoom error and reformatting popup

--- js/map.js
+++ js/map.js
@@ -10,7 +10,7 @@ function setLayerStyle(topic,featureAtt){
 
 function createMap(postcodes){
 	let map = L.map('map').setView([52,0], 6);
-	map.options.minZoom = 10;
+	map.options.minZoom = 7;
 
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -27,6 +27,33 @@ function createMap(postcodes){
 
 	let postcodeLayer = getPostcodeLayer(postcodes);
 	postcodeLayer.addTo(map);
+	
+
+    let info = L.control();
+
+    info.onAdd = function (map) {
+	    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+	    this.update();
+	    return this._div;
+	};
+
+	info.update = function (props) {
+    this._div.innerHTML = '<p id="zoomwarning">Please zoom in to see map layers</p>';
+	};
+
+	info.addTo(map);
+	info.update();
+
+	map.on('zoomend', function() {
+    	let zoom = map.getZoom();
+    	if(zoom<10){
+    		$('.info').show();
+    	} else {
+    		$('.info').hide();
+    	}
+
+	});
+
 	let bounds = postcodeLayer.getBounds();
 	map.fitBounds(bounds);
 }
@@ -35,10 +62,15 @@ function getPostcodeLayer(postcodes,index){
 	let markers = [];
 	postcodes.forEach(function(postcode){
 		let popUp = "<p>Postcode: "+postcode[0]+"</p>";
-		popUp += "<p>IMD rank: "+postcode[5]+"</p>";
-		popUp += "<p>IMD Decile: "+getDecile(postcode[5])+"</p>";
-		popUp += "<p>Income rank: "+postcode[6]+"</p>";
-		popUp += "<p>Income Decile: "+getDecile(postcode[6])+"</p>";
+		popUp += '<p class="popuptext">IMD rank: '+postcode[5]+' out of 32,844</p>';
+		popUp += '<p class="popuptext">IMD Decile: '+getDecile(postcode[5])+'</p>';
+		popUp += '<p class="popuptext">Income Decile: '+getDecile(postcode[6])+'</p>';
+		popUp += '<p class="popuptext">Employment Decile: '+getDecile(postcode[7])+'</p>';
+		popUp += '<p class="popuptext">Education Decile: '+getDecile(postcode[8])+'</p>';
+		popUp += '<p class="popuptext">Health and Disability Decile: '+getDecile(postcode[9])+'</p>';
+		popUp += '<p class="popuptext">Crime Decile: '+getDecile(postcode[10])+'</p>';
+		popUp += '<p class="popuptext">Housing and Services Decile: '+getDecile(postcode[11])+'</p>';
+		popUp += '<p class="popuptext">Living Environment Decile: '+getDecile(postcode[12])+'</p>';
 		let marker = L.marker([postcode[1], postcode[2]]).bindPopup(popUp);
 		markers.push(marker);
 	});
