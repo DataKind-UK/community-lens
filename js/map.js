f557e56b2fc9d1cbb8bfc8b84debd5785a33a438
Caitlin Loftus <caitlinmloftus@gmail.com>
2025-09-25 16:09:58 +0100

    fixing broken imd api call

--- js/map.js
+++ js/map.js
@@ -17,7 +17,7 @@ function createMap(postcodes){
 	}).addTo(map);
 
 	featureLayer = L.esri.featureLayer({
-	  url: 'https://services3.arcgis.com/ivmBBrHfQfDnDf8Q/arcgis/rest/services/Indices_of_Multiple_Deprivation_(IMD)_2019/FeatureServer/0',
+	  url: 'https://services-eu1.arcgis.com/EbKcOS6EXZroSyoi/arcgis/rest/services/Indices_of_Multiple_Deprivation_%28IMD%29_2019/FeatureServer/0/',
 	  simplifyFactor: 0.35,
 	  minZoom:10,
 	  precision: 5,
